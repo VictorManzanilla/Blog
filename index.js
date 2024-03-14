af0a3cf831a6b8f98df80a2dd1ef17e59f13d179
Victor <vmanzanilla7@gmail.com>
2024-03-13 21:31:30 -0500

    initial node, express, ejs, static files, and get route set up

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req,res) => {
+    res.render('index.ejs')
+})
+//Don't know if I will use this route
+app.get('/about', (req, res) => {
+    res.render('about.ejs')
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}!`);
+})
